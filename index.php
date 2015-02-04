a877b5c52136d6f299b7d18adc8381421cc426be
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-04 20:10:05 +0000

    Add Menu

--- index.php
+++ index.php
@@ -7,23 +7,51 @@
 		<meta name="viewport" content="width=device-width">
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<script src="js/modernizr.js"></script> 
+		<script src="js/prefixfree.min.js"></script>
 	</head>
 	<body>
 		<header class="header">
-			<a href="index.html" id="logo">
+			<a href="index.php" id="logo">
 				<img src="http://www.gravatar.com/avatar/57cb4a5232c5a0c5bc4785fbf34a9182.png" width="100" height="100" class="avatar" />
-				<h1>Dave Wakely</h1>
+				<div class="strapline">Dav<span>e</span> W<span>a</span>kely</h1></div>
 				<h2>Developer</h2>
 			</a>
-			<nav>
-				<!-- Nav Links Goes Here -->
+			<nav id="menu">
+				<div class="slider">
+					<input id="home" name="view" type="radio" checked>
+					<label for="home" onclick="">Home</label>
+
+					<input id="about" name="view" type="radio">
+					<label for="about" onclick="">About</label>
+					
+					<input id="portfolio" name="view" type="radio">
+					<label for="portfolio" onclick="">Portfolio</label>
+
+					<input id="contact" name="view" type="radio">
+					<label for="contact" onclick="">Contact</label>
+
+					<input id="links" name="view" type="radio">
+					<label for="links" onclick="">Other Sites</label>
+
+					 <span class="button"></span>
+				</div>
 			</nav>
 		</header>
 		<section class="wrapper">
 			<div class="row">
-				<div class="column-3"></div>
-				<div class="column-3"></div>
-				<div class="column-6"></div>
+				<div class="column-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu magna ornare, suscipit tellus blandit, blandit augue. Aliquam fermentum ex dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse diam quam, dictum ac interdum vel, viverra sit amet risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc pretium placerat leo eu molestie. Mauris vehicula at sem sit amet accumsan. Vestibulum augue turpis, sodales sed eros vitae, pretium congue ex. Quisque facilisis elit tortor, vel cursus felis molestie vel. Mauris ac iaculis magna. Sed vel ante laoreet, facilisis nisl vel, elementum urna. In et lacinia lectus, vitae condimentum tortor. Donec purus purus, volutpat et enim ut, hendrerit tempor enim.
+
+Nam hendrerit arcu urna. Morbi nec tellus interdum, gravida augue vitae, tincidunt nulla. Nunc fermentum consequat justo, id vehicula ipsum fermentum nec. Nullam vel elit vitae magna pellentesque aliquet. Curabitur ac ligula ac tortor vestibulum tempor ut ut erat. Quisque non elit sed arcu consequat lacinia. Aenean sodales lectus ut nisl vulputate suscipit.
+
+Nulla facilisi. Donec at venenatis mi. Nulla in finibus metus. In tristique scelerisque tempor. Donec tristique, erat eu sollicitudin varius, nibh elit tincidunt turpis, in imperdiet odio odio non erat. Nam eget ullamcorper sapien. Nullam nibh arcu, mollis sit amet nibh at, suscipit pulvinar dolor. Fusce commodo rutrum elementum. Aenean eget turpis id sem ultricies eleifend. Nulla diam dolor, iaculis at purus non, dignissim mollis sapien. Mauris lobortis elementum purus quis suscipit. Fusce et lectus lorem. Quisque nulla tellus, suscipit quis fermentum volutpat, sodales eu justo.
+
+Aliquam elementum elit eu magna imperdiet, ac commodo magna lobortis. Maecenas et placerat ante, non sollicitudin urna. Proin ut sem mattis, porttitor metus elementum, hendrerit mauris. Mauris volutpat, enim et luctus luctus, tellus odio mollis elit, vehicula rhoncus libero justo vitae tortor. Sed lobortis elit justo, quis commodo metus eleifend sit amet. Vestibulum et consectetur odio, posuere luctus sapien. Nulla et pellentesque tortor, eget laoreet odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque mattis id nisl in vehicula. Etiam ac rhoncus est. Proin vel mauris lacus. Nullam blandit velit nec euismod aliquet. Maecenas enim urna, accumsan pellentesque scelerisque eu, pretium eget orci. Aliquam porttitor, turpis et bibendum ornare, odio quam rutrum nisl, nec porta massa orci nec orci. Quisque tempor ipsum in egestas efficitur.
+
+Vivamus id risus nec nisi sollicitudin semper in nec arcu. Maecenas dui tortor, posuere ut suscipit vel, cursus sit amet nunc. Morbi eget purus risus. Ut sit amet purus et lectus ornare luctus eget vel enim. Sed quis erat non orci aliquet hendrerit ac a lorem. Aenean iaculis elit odio, sit amet elementum justo commodo nec. Cras vitae ex massa. Aenean ullamcorper sem eget ornare dictum. Donec suscipit luctus risus, in porta urna lacinia ac. Duis maximus pharetra urna sit amet hendrerit. Quisque feugiat porta volutpat. Integer gravida dictum ligula eu volutpat. Suspendisse quis lacinia quam.</div>
+				<div class="column-10">50%</div>
+			</div>
+			<div class="row">
+				<div class="column-2">10%</div>
 			</div>
 		</section>
 		<footer>

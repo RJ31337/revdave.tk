5cd1e34d00987c4e83a0d58ae30fcfbf804c7e89
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-04 14:56:18 +0000

    Add Fonts

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Dave Wakely | Developer</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<script src="js/modernizr.js"></script> 
+	</head>
+	<body>
+		<header>
+			<a href="index.html" id="logo">
+				<h1>Dave Wakely</h1>
+				<h2>Developer</h2>
+			</a>
+			<nav>
+				<!-- Nav Links Goes Here -->
+			</nav>
+		</header>
+		<div role="main">
+
+ 		</div>
+		<footer>
+			<a href="http://twitter.com/rj31337"><img src="img/twitter-wrap.png" alt="Twitter Logo"></a>
+			<a href="https://www.facebook.com/revdave31337"><img src="img/facebook-wrap.png" alt="Facebook Logo"></a>
+				<p>&copy; 2015 Dave Wakely.</p>
+		</footer>
+	</body>
+</html>

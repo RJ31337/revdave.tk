8bd6532c7b9b9b91159dd05dea4bc955d138814f
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-04 17:33:21 +0000

    Replace Susy with Custom Basic Grid System

--- index.php
+++ index.php
@@ -9,8 +9,9 @@
 		<script src="js/modernizr.js"></script> 
 	</head>
 	<body>
-		<header>
+		<header class="header">
 			<a href="index.html" id="logo">
+				<img src="http://www.gravatar.com/avatar/57cb4a5232c5a0c5bc4785fbf34a9182.png" width="100" height="100" class="avatar" />
 				<h1>Dave Wakely</h1>
 				<h2>Developer</h2>
 			</a>
@@ -18,9 +19,13 @@
 				<!-- Nav Links Goes Here -->
 			</nav>
 		</header>
-		<div role="main">
-
- 		</div>
+		<section class="wrapper">
+			<div class="row">
+				<div class="column-3"></div>
+				<div class="column-3"></div>
+				<div class="column-6"></div>
+			</div>
+		</section>
 		<footer>
 			<a href="http://twitter.com/rj31337"><img src="img/twitter-wrap.png" alt="Twitter Logo"></a>
 			<a href="https://www.facebook.com/revdave31337"><img src="img/facebook-wrap.png" alt="Facebook Logo"></a>
